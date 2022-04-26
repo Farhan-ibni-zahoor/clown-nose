c248aab943e7203e11ac3518f152c3f05264e73b
Farhan-ibni-zahoor <87404631+Farhan-ibni-zahoor@users.noreply.github.com>
2022-04-26 07:13:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preaload() {
+};
+
+function setup() {
+    create = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+}
